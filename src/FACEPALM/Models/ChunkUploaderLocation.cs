5a5c08b605e7b55cb3b94fb85bb6ce096b1e7e58
Shashanka DAS <gudurajasekarreddy@gmail.com>
2024-11-24 16:11:47 +0530

    Added model to store chunk info and locatiom

---
+++ src/FACEPALM/Models/ChunkUploaderLocation.cs
@@ -0,0 +1,21 @@
+using Commons.Interfaces;
+using Npgsql;
+using Uploader.Enums;
+
+namespace FACEPALM.Models;
+
+public class ChunkUploaderLocation(string serializedName, int chunkIndex, StorageProviderTypes providerType) : IDatabaseModels
+{
+    public string SerializedName { get; set; } = serializedName;
+    public int ChunkIndex { get; set; } = chunkIndex;
+    public StorageProviderTypes ProviderType { get; set; } = providerType;
+    
+    public static ChunkUploaderLocation Deserialize(NpgsqlDataReader reader)
+    {
+        var serializedName = reader.GetString(0);
+        var chunkIndex = reader.GetInt32(1);
+        var providerType = (StorageProviderTypes)reader.GetInt32(2);
+
+        return new ChunkUploaderLocation(serializedName, chunkIndex, providerType);
+    }
+}
--- src/Uploader/Program.cs
+++ src/Uploader/Program.cs
@@ -1,11 +1,9 @@
-using Uploader.Models;
-
 namespace Uploader;
 
 public static class Program
 {
     private static void Main()
     {
-        throw new NotImplementedException();
+        throw new NotSupportedException();
     }
 }
