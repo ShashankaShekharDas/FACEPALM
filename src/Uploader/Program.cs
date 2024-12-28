cabd6d901a02338fcd68f6ebd240d40fb44737a1
Shashanka DAS <gudurajasekarreddy@gmail.com>
2024-12-28 15:10:51 +0530

    Push (broken) editorconfig and fix a small issue

--- src/Uploader/Program.cs
+++ src/Uploader/Program.cs
@@ -1,12 +1,8 @@
-using Uploader.Enums;
-using Uploader.Models;
-using Uploader.Uploaders;
-
 namespace Uploader
 {
     public static class Program
     {
-        private static async Task Main()
+        private static void Main()
         {
             throw new NotImplementedException();
         }
